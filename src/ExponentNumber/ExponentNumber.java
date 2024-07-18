03ddb4342862d931408f9bb4b7d02d9be7e14e76
MertSener <mehmetmertsener@hotmail.com>
2024-07-18 21:39:25 +0300

    Create ExponentNumber.java

---
+++ src/ExponentNumber/ExponentNumber.java
@@ -0,0 +1,17 @@
+package ExponentNumber;
+import java.util.Scanner;
+public class ExponentNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.print("Enter a exponent");
+        int exp = sc.nextInt();
+        int square=1;
+
+        for (int i = 1; i <= exp; i++) {
+            square=square*num;
+        }
+        System.out.print(square);
+    }
+}
